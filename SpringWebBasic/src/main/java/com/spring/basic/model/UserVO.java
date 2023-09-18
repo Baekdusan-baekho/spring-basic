83a7f6530c742654723371d6db09204e741e160d
honghong <whdtjs152413@gmail.com>
2023-09-18 17:31:54 +0900

    230918 spring구조

---
+++ SpringWebBasic/src/main/java/com/spring/basic/model/UserVO.java
@@ -0,0 +1,29 @@
+package com.spring.basic.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter @Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+
+public class UserVO {
+
+	
+	private String userId;
+	private String userPw;
+	private String userName;
+	private List<String> hobby;
+	// 자바 규칙 
+	//1.은닉 구현    2.기본 생성자 필수  3. getter에는 따로 매개변수가 존재하지 않아야 한다. 4. public으로 열려있어야 한다.        
+
+}
