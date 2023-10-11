f635f83bcf04528032000fd26e344c6f7fa86e7b
honghong <whdtjs152413@gmail.com>
2023-10-11 18:32:53 +0900

    231011 가상환경 sql 테스트 배우고 있음

---
+++ MyWeb/src/main/java/com/spring/myweb/freeboard/controller/FreeBoardController.java
@@ -0,0 +1,45 @@
+package com.spring.myweb.freeboard.controller;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.myweb.freeboard.dto.FreeRegistRequestDTO;
+import com.spring.myweb.freeboard.service.IFreeBoardService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/freeboard")
+@RequiredArgsConstructor
+public class FreeBoardController {
+
+	private final IFreeBoardService service;
+	
+	//목록 화면
+	@GetMapping("/freeList")
+	public void freeList(Model model) {
+		System.out.println("/freeboard/freeList: GET!");
+		
+		model.addAttribute("boardList", service.getList());
+	}
+	
+	
+	//글 등록 처리
+	@PostMapping("/freeRegist")
+	public String regist(FreeRegistRequestDTO dto) {
+		service.regist(dto);
+		return "redirect:/freeboard/freeList";
+	}
+	
+	@GetMapping("/content")
+	public String content(int bno, Model model) {
+		System.out.println("/freeboard/content: GET!");
+		model.addAttribute("bno", service.getContent(bno));
+		return "redirect:/freeboard/freeList";
+	}
+	
+}
